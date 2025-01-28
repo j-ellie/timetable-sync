6d039a1eec6ebc675149e8daa85bfb24a042675f
James <james@jamesz.dev>
2025-01-28 16:30:44 +0000

    change position of version on footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,8 @@ export default function Footer() {
       <Box mt={5}>
         <footer>
           {/* <Text textAlign="center" fontSize="sm">Timetable Sync is not affiliated with DCU. Made by <a href="https://jamesz.dev" target='_blank' style={{ textDecoration: "underline", fontWeight: "bold"}}>James</a> </Text> */}
-          <Text textAlign="center" fontSize="sm">Timetable Sync is not affiliated with DCU. Made by <a href="#" style={{ textDecoration: "underline", fontWeight: "bold"}}>Ellie</a> </Text>
+          <Text textAlign="center" fontSize="sm">Timetable Sync is not affiliated with DCU. Made by <a href="https://elliee.me?utm_source=ts" style={{ textDecoration: "underline", fontWeight: "bold"}} target="_blank">Ellie</a> (V: {__APP_VERSION__})</Text>
           <Text textAlign="center" fontSize="sm">By using this app, you agree to our: <a href="/privacy" style={{ textDecoration: "underline", fontWeight: "bold"}}>Privacy Policy</a></Text>
-          <Text>V: {__APP_VERSION__}</Text>
         </footer>
       </Box>
     )
